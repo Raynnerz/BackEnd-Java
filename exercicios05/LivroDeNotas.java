f9c05bef87782269ce46d7098a44cb4ddcb9e99e
Raynnerz <102298525+Raynnerz@users.noreply.github.com>
2022-10-03 17:31:27 -0300

    Add files via upload

---
+++ exercicios05/LivroDeNotas.java
@@ -0,0 +1,28 @@
+//alta coesão
+
+public class LivroDeNotas{
+    //variável de instância
+    //instância é sinônimo de objeto
+    //modificador de acesso: private
+    private String nomeDisciplina;
+
+    //método modificador
+    //setter
+    public void setNomeDisciplina(String nomeDisciplina){
+        //operador de auto referência: this
+        if (nomeDisciplina.length() >= 10)
+            this.nomeDisciplina = nomeDisciplina;
+    }
+
+    //método de acesso
+    //getter
+    public String getNomeDisciplina(){
+        return nomeDisciplina;
+    }
+
+
+    //método
+    public void exibirMensagem(){
+        System.out.println ("Bem vindo ao livro de notas da disciplina " + nomeDisciplina);    
+    } 
+}
---
+++ exercicios05/TesteLivroDeNotas.java
@@ -0,0 +1,43 @@
+import static javax.swing.JOptionPane.showInputDialog;
+import javax.swing.JOptionPane;
+public class TesteLivroDeNotas {
+    public static void main(String[] args) {
+
+        String nomeDaDisciplina = JOptionPane.showInputDialog("Qual o nome da disciplina");
+        //variável de referência
+        LivroDeNotas livroDeNotas;
+
+        //um objeto sendo construído
+        livroDeNotas = new LivroDeNotas();
+        //livroDeNotas.nomeDaDisciplina = nomeDaDisciplina;
+        livroDeNotas.setNomeDisciplina(nomeDaDisciplina);
+
+        //System.out.println(livroDeNotas.nomeDisciplina);
+        System.out.println(livroDeNotas.getNomeDisciplina());
+
+        livroDeNotas.exibirMensagem();
+
+
+        LivroDeNotas livro2 = new LivroDeNotas();
+        //syntax sugar
+        //livro2.nomeDaDisciplina = "Modelagem";
+        livro2.setNomeDisciplina("Modelagem");
+
+
+        nomeDaDisciplina = showInputDialog("Outra disciplina");
+
+        livroDeNotas = new LivroDeNotas();
+        
+        //você não quer fazer isso
+        System.gc();
+
+        
+
+        // while(true){
+        //     new LivroDeNotas();
+        // }
+
+
+
+    }
+}
